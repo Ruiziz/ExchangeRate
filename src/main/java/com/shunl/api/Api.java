62f5774288a95e00a2ad6c061ca76261706037d9
shunl <lssvwp@gmail.com>
2019-11-24 13:54:01 -0500

    Add javadoc and a new method

--- src/main/java/com/shunl/api/Api.java
+++ src/main/java/com/shunl/api/Api.java
@@ -19,7 +19,11 @@ public class Api {
 	
 	
 	static String url_str = "https://api.exchangerate-api.com/v4/latest/";
-
+	/**
+	 * This is the API we want to use to get currency exchange rate.
+	 * @param currency The Currency you want to exchange
+	 * @throws Exception
+	 */
     public Api(String currency) throws Exception {
 
     	// Setting URL
@@ -27,7 +31,11 @@ public class Api {
     	
     	this.real_url = new URL(url);
     }
-    
+    /**
+	 * Get the currency info
+	 * @return a JsonObject includes date, rate, etc.
+	 * @throws IOException
+	 */
     public JsonObject getCurrencyInfo() throws IOException {
     	HttpURLConnection request = (HttpURLConnection) real_url.openConnection();
     	request.connect();
@@ -38,18 +46,39 @@ public class Api {
     	
     	return jsonobj;
     }
-    
+    /**
+	 * Get exchange rates for 52 countries.
+	 * @param jsonobj 
+	 * @param currency The currency you want to exchange to.
+	 * @return
+	 */
     public Double getRate(JsonObject jsonobj,String currency) {
     	JsonObject req_result = jsonobj.get("rates").getAsJsonObject();
     	Double result = req_result.get(currency).getAsDouble();
     	return result;
     }
-	
+	/**
+	 * get country list as a set
+	 * @return a set of countries.
+	 * @throws Exception
+	 */
 	public Set<String> getCountry() throws Exception {
-		Getcountry country = new Getcountry();
-		return country.countryList;
+		JsonObject jsonobj = getCurrencyInfo();
+        //JsonElement countries = jsonobj.get("rates");
+        JsonObject countries2 = jsonobj.getAsJsonObject("rates");
+        Set<String> countryList = countries2.keySet();
+		return countryList;
 	}
-    
+	/**
+	 * Get update date as string
+	 * @return a string showing the update date
+	 * @throws IOException
+	 */
+	public String getUpdateDate() throws IOException {
+		JsonObject jsonobj = getCurrencyInfo();
+		String date = jsonobj.get("date").getAsString();
+		return date;
+	}
     
 
 }
---
+++ src/main/java/com/shunl/api/ApiFunctionTest.java
@@ -0,0 +1,19 @@
+package com.shunl.api;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+public class ApiFunctionTest {
+
+    public static void main(String[] args) throws Exception {
+        Api api = new Api("USD");
+        JsonObject jsonobj = api.getCurrencyInfo();
+        String date = api.getUpdateDate();
+        System.out.println(date);
+        
+    }
+        
+    
+        
+    
+    
+} 
